0821b576d3fc3b64a07e6b5e5e9f163bce3fd680
Jose Molina <mol14492@uvg.edu.gt>
2016-09-21 09:19:38 -0600

    Clase BinaryTree

---
+++ BinaryTree.java
@@ -0,0 +1,52 @@
+import java.util.Iterator;
+
+/*
+ * Clase BinaryTree, se tomo como ejemplo para su realizacion
+ * la clase del libro Java Structures, capitulo 12.4.1 
+ * 
+ */
+public class BinaryTree <E> {
+
+	public BinaryTree() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public BinaryTree(E valor) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BinaryTree(E valor, BinaryTree<E> left, BinaryTree<E> right) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public BinaryTree<E> left(){
+		
+	}
+	
+	public BinaryTree<E> parent(){
+		
+	}
+	
+	public void setLeft(BinaryTree<E> newLeft){
+		
+	}
+	
+	public void setParent(BinaryTree<E> newParent){
+		
+	}
+	public Iterator <E> iterator(){
+		
+	}
+	
+	public boolean isLeftChild(){
+		
+	}
+	
+	public E value(){
+		
+	}
+	public void setValue(E value){
+		
+	}
+
+}
