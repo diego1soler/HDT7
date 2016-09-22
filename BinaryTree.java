506c7615e3600d4810d2ea6db124cedb6b8883e1
Jose Molina <mol14492@uvg.edu.gt>
2016-09-21 19:55:31 -0600

    Add files via upload

--- BinaryTree.java
+++ BinaryTree.java
@@ -5,47 +5,110 @@ import java.util.Iterator;
  * la clase del libro Java Structures, capitulo 12.4.1 
  * 
  */
-public class BinaryTree <E> {
+public class BinaryTree<E>{
+	
+protected E val;
+protected BinaryTree<E> parent,left,right;
+
 
 	public BinaryTree() {
-		// TODO Auto-generated constructor stub
+		// Se genera un nodo vacio.
+		
+		val = null;
+		parent = null;
+		left=right=this;
 	}
 	
 	public BinaryTree(E valor) {
 		// TODO Auto-generated constructor stub
+		
+		val = valor;
+		right=left= new BinaryTree<E>();
+		setLeft(left);
+		setRight(right);
+		
 	}
 
 	public BinaryTree(E valor, BinaryTree<E> left, BinaryTree<E> right) {
 		// TODO Auto-generated constructor stub
+		val=valor;
+		if(left==null){
+			left = new BinaryTree<E>();
+		}
+		
+		setLeft(left);
+		
+		if(right == null){
+			right = new BinaryTree<E>();
+		}
+		
+		setRight(right);
+		
 	}
 	
 	public BinaryTree<E> left(){
+		
+		return left;
 		
 	}
 	
 	public BinaryTree<E> parent(){
 		
+		return parent;
+	}
+	
+	public BinaryTree<E> right(){
+		
+		return right;
+		
 	}
 	
 	public void setLeft(BinaryTree<E> newLeft){
 		
+		if(isEmpty()){
+			return;
+		}
+		if (left != null && left.parent() == this){
+			left.setParent(null);
+			left = newLeft;
+			left.setParent(this);
+		}
+	}
+	
+	public void setRight(BinaryTree<E> newRight){
+		
+		if(isEmpty()){
+			return;
+		}
+		if (right != null && right.parent() == this){
+			right.setParent(null);
+			right = newRight;
+			right.setParent(this);
+		}
 	}
 	
 	public void setParent(BinaryTree<E> newParent){
-		
+		if (!isEmpty()){
+			parent=newParent;
+		}
 	}
+	// METODO NECESITA CORRECCION
 	public Iterator <E> iterator(){
 		
 	}
 	
-	public boolean isLeftChild(){
-		
-	}
 	
 	public E value(){
+		return val;
 		
 	}
-	public void setValue(E value){
+	public void setValue(E valor){
+		val=valor;
+	}
+	// CORREGIR ESTE METODO
+	public boolean isEmpty(){
+		
+			return true;
 		
 	}
 
