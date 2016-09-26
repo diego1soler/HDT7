5e30c837b0412353a153dbdf5456100fcd1f12ae
diego1soler <dsoler1998@gmail.com>
2016-09-26 16:41:41 -0600

    Actualizado el metodo de leer archivo

--- Association.java
+++ Association.java
@@ -1,88 +1,60 @@
-
 /**
  * Clase Asociation, se tomo como referncia la clase de la pagina
  * https://github.com/sliuu/post_script/blob/master/structure5/Association.java
  */
-import java.util.Map;
+
 import java.io.*;
 
 
-public class Association<K,V> implements Map.Entry<K,V>
-{
-
-    protected K tKey; 
-    protected V tValor; 
+public class Association<K,V> {
+	protected K english;
+	protected V spanish;
+	//Metodos 
+	public Association()
+	// post: constructor that generates an empty node
+	{
+		english = null;
+		spanish = null;
+	}
+	public Association(K english,  V spanish,Association<K,V> words){
+		this.english= english;
+		this.spanish=spanish;
+              
+		}
+	//Metodos Sets
+	public void setEnglish(K word){
+		english=word;
+	}
+	public void setSpanish(V word){	
+		spanish=word;
+	}
+	public K getEnglish(){	
+		return english;
+	}
+	public V getSpanish(){	
+		return spanish;
+	}
     
-    public Association(K key, V valor)
-    {
-        tKey = key;
-        tValor = valor;
-    }
-
- 
-    public Association(K key)
-    {
-        this(key,null);
-    }
-
-
-    public boolean equals(Object other)
-    {
-        Association otherAssoc = (Association)other;
-        return getKey().equals(otherAssoc.getKey());
-    }
-    
-
-    public int hashCode()
-    {
-        return getKey().hashCode();
-    }
-    
-
-    public V getValue()
-    {
-        return tValor;
-    }
-
-
-    public K getKey()
-    {
-        return tKey;
-    }
-
-
-    public V setValue(V value)
-    {
-        V oldValue = tValor;
-        tValor = value;
-        return oldValue;
-    }
-
-
-    public String toString()
-    {
-        StringBuffer s = new StringBuffer();
-        s.append("<Association: "+getKey()+"="+getValue()+">");
-        return s.toString();
-    }
-    
-    public String leerArchivo(String direccion){
-
-    		String resultado = "";
-    		try {
-    			FileReader f = new FileReader(direccion);
-    			BufferedReader b = new BufferedReader(f);
-    			try {
-    				resultado = b.readLine();
-    				b.close();
-    			} catch (IOException e) {
-    				return "No se puede abrir archivo";
-    			}
-    		} catch (FileNotFoundException e) {
-    			return "Archivo no encontrado";
-    		}
-    		return resultado;
-    	}
+    public String leerArchivo (String direccion) {
+		String res = "";
+		String temp = "";
+		try {
+			FileReader f = new FileReader(direccion);
+			BufferedReader b = new BufferedReader(f);
+			try {
+				while ((temp = b.readLine()) != null) {
+					res += temp + "-";
+				}
+				b.close();
+			} catch (IOException e) {
+				return "";
+			}
+		} catch (FileNotFoundException e) {
+			return "";
+		}
+		return res;
+	}
     
 
 }
+   
